d1b91c4f495e7d415ffb04e94c23f40c47927732
SatoshiUmeda1803 <jartashi0911@gmail.com>
2022-07-14 19:36:57 +0900

    環境変数の表記を修正

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -4,6 +4,7 @@ class RoutesController < ApplicationController
     require 'net/http'
     require 'openssl'
     
+    # 希望到着時刻を受け取って、出発時刻を計算
     goal_time = params[:goal_time]
     params = URI.encode_www_form({goal_time: goal_time})
     url = URI("https://navitime-route-totalnavi.p.rapidapi.com/route_transit?start=35.665251%2C139.712092&goal=35.661971%2C139.703795&#{params}&datum=wgs84&term=1440&limit=5&coord_unit=degree")
