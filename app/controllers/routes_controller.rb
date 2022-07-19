c565d196e78a9dae4a818b6959151bdfd5006cbe
SatoshiUmeda1803 <jartashi0911@gmail.com>
2022-07-19 22:05:05 +0900

    fix: 入力された到着時間より、20分早い時間でルート計算するように変更

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -25,7 +25,7 @@ class RoutesController < ApplicationController
     start_hash = start_info.values_at('lat','lon')
     @start_lat_lon = start_hash.join(',')
     start_params = URI.encode_www_form({start: @start_lat_lon})
-    
+
     # 到着地の住所を緯度経度で取得
     goal_point = params[:goal_point]
     goal_encoded = URI.encode_www_form({word: goal_point})
@@ -42,10 +42,10 @@ class RoutesController < ApplicationController
     @goal_lat_lon = goal_hash.join(',')
     goal_params = URI.encode_www_form({goal: @goal_lat_lon})
     # 希望到着時刻を受け取って、出発時刻を計算
-    goal_time = params[:goal_time]
-    goal_time_params = URI.encode_www_form({goal_time: goal_time})
+    foge_goal_time = params[:goal_time]
+    fast_goal_time = Time.parse(params[:goal_time]).ago(20.minutes).strftime("%FT%R")
+    goal_time_params = URI.encode_www_form({goal_time: fast_goal_time})
     url = URI("https://navitime-route-totalnavi.p.rapidapi.com/route_transit?#{start_params}&#{goal_params}&#{goal_time_params}&datum=wgs84&term=1440&limit=5&coord_unit=degree")
-      
     request = Net::HTTP::Get.new(url)
     request["X-RapidAPI-Key"] = ENV["NAVITIME_KEY"]
     request["X-RapidAPI-Host"] = 'navitime-route-totalnavi.p.rapidapi.com'
@@ -54,7 +54,6 @@ class RoutesController < ApplicationController
     hash = JSON.parse(response.body)
     start_time = hash["items"][0]["summary"]["move"]["from_time"]
     @start_time = Time.parse(start_time).strftime('%-m/%d %H:%M')
-
     #ルートの形状を取得
     url = URI("https://navitime-route-totalnavi.p.rapidapi.com/shape_transit?#{start_params}&#{goal_params}&#{goal_time_params}&options=transport_shape&format=geojson&term=1440&limit=5&datum=wgs84&coord_unit=degree")
 
