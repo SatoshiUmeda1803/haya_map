447300fabfde395e2f03d5681cff003598c3603a
SatoshiUmeda1803 <jartashi0911@gmail.com>
2022-07-24 11:42:20 +0900

    add: フラッシュメッセージを表示するようにしました

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,19 +1,22 @@
 class UserSessionsController < ApplicationController
+  protect_from_forgery
+
   def new; end
 
   def create
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to(root_path, notice: 'ログインしました')
+      redirect_back_or_to root_path, success: 'ログインしました'
     else
-      flash.now[:alert] = 'ログインできませんでした'
+      flash.now[:danger] = 'ログインできませんでした'
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to(login_path, notice: 'ログアウトしました')
+    redirect_back_or_to login_path, success: 'ログアウトしました'
   end
+
 end
