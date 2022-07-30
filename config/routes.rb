bf591bbd47b39894fa6c42ae47ec2352ebab30a3
SatoshiUmeda1803 <jartashi0911@gmail.com>
2022-07-30 18:35:07 +0900

    add:パスワードをメールでリセットできるように実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/create'
+  get 'password_resets/edit'
+  get 'password_resets/update'
   get 'time_settings/edit'
   root to: 'routes#new'
 
@@ -9,4 +13,8 @@ Rails.application.routes.draw do
   resource :user, only: %i[new create]
   resource :time_setting, only: %i[edit update]
   resource :route, only: %i[new show]
+
+  resources :password_resets, only: %i[new create edit update]
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_080536) do
+ActiveRecord::Schema.define(version: 2022_07_30_061232) do
 
   create_table "time_settings", force: :cascade do |t|
     t.boolean "randomized", default: false, null: false
@@ -28,7 +28,12 @@ ActiveRecord::Schema.define(version: 2022_07_25_080536) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
