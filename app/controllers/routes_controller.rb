2bab6e8e531ee00a48a521785c5c67eb9b486445
SatoshiUmeda1803 <jartashi0911@gmail.com>
2022-08-10 11:11:39 +0900

    fix: 住所検索をgoogle apiに変更し、駅名、建物名、曖昧検索に対応

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -14,33 +14,24 @@ class RoutesController < ApplicationController
     end
 
     start_encoded = URI.encode_www_form({word: start_point})
-    url = URI("https://navitime-geocoding.p.rapidapi.com/address?coord_unit=degree&datum=wgs84&limit=10&#{start_encoded}&sort=code_asc&offset=0")
+    url = URI("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{start_encoded}&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=#{ENV["GOOGLE_KEY"]}")
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     request = Net::HTTP::Get.new(url)
-    request["X-RapidAPI-Key"] = ENV["NAVITIME_KEY"]
-    request["X-RapidAPI-Host"] = 'navitime-geocoding.p.rapidapi.com'
-
     response = http.request(request)
     hash = JSON.parse(response.body)
-  
-    #入力された住所から情報を取得できなかった場合
-    if hash["count"]["total"] == 0
-      flash.now[:danger] = '出発地の住所を正確に入力してください'
-      render :new
-      return
-    end
+ 
     
     #入力された住所から情報を取得できた場合
-    start_info = hash["items"][0]["coord"]
-    start_hash = start_info.values_at('lat','lon')
+    start_info = hash["candidates"][0]["geometry"]["location"]
+    start_hash = start_info.values_at('lat','lng')
     @start_lat_lon = start_hash.join(',')
     start_params = URI.encode_www_form({start: @start_lat_lon})
+    @start_name = hash["candidates"][0]["name"]
       
-
     # 到着地の住所を緯度経度で取得
     goal_point = params[:goal_point]
     if goal_point == ""
@@ -50,28 +41,22 @@ class RoutesController < ApplicationController
     end
 
     goal_encoded = URI.encode_www_form({word: goal_point})
-    url = URI("https://navitime-geocoding.p.rapidapi.com/address?coord_unit=degree&datum=wgs84&limit=10&#{goal_encoded}&sort=code_asc&offset=0")
+    url = URI("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{goal_encoded}&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=#{ENV["GOOGLE_KEY"]}")
 
     request = Net::HTTP::Get.new(url)
-    request["X-RapidAPI-Key"] = ENV["NAVITIME_KEY"]
-    request["X-RapidAPI-Host"] = 'navitime-geocoding.p.rapidapi.com'
-
     response = http.request(request)
     hash = JSON.parse(response.body)
-    #入力された住所から情報を取得できなかった場合
-    if hash["count"]["total"] == 0
-      flash.now[:danger] = '到着地の住所を正確に入力してください'
-      render :new
-      return
-    end
 
-    goal_info = hash["items"][0]["coord"]
-    goal_hash = goal_info.values_at('lat','lon')
+    goal_info = hash["candidates"][0]["geometry"]["location"]
+    goal_hash = goal_info.values_at('lat','lng')
     @goal_lat_lon = goal_hash.join(',')
     goal_params = URI.encode_www_form({goal: @goal_lat_lon})
+    @goal_name = hash["candidates"][0]["name"]
 
     # 希望到着時刻を受け取る
     goal_time = params[:goal_time]
+    @goal_time = Time.parse(goal_time)
+    
     if goal_time == ""
       flash.now[:danger] = '到着時刻が設定されていません'
       render :new
@@ -83,19 +68,24 @@ class RoutesController < ApplicationController
       if current_user.time_setting.randomized == true
         random = Random.rand(9)
         if random == 1
-          fast_goal_time = Time.parse(goal_time).strftime("%FT%R")
+          fast_goal_time = @goal_time.strftime("%FT%R")
         else
-          fast_goal_time = Time.parse(goal_time).ago(current_user.time_setting.costom_time.minutes).strftime("%FT%R") 
+          fast_goal_time = @goal_time.ago(current_user.time_setting.costom_time.minutes).strftime("%FT%R") 
         end
       else
-        fast_goal_time = Time.parse(goal_time).ago(current_user.time_setting.costom_time.minutes).strftime("%FT%R")
+        fast_goal_time = @goal_time.ago(current_user.time_setting.costom_time.minutes).strftime("%FT%R")
       end
     else
-      fast_goal_time = Time.parse(goal_time).ago(20.minutes).strftime("%FT%R")
+      fast_goal_time = @goal_time.ago(20.minutes).strftime("%FT%R")
     end
     
     goal_time_params = URI.encode_www_form({goal_time: fast_goal_time})
     url = URI("https://navitime-route-totalnavi.p.rapidapi.com/route_transit?#{start_params}&#{goal_params}&#{goal_time_params}&datum=wgs84&term=1440&limit=5&coord_unit=degree")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
     request = Net::HTTP::Get.new(url)
     request["X-RapidAPI-Key"] = ENV["NAVITIME_KEY"]
     request["X-RapidAPI-Host"] = 'navitime-route-totalnavi.p.rapidapi.com'
@@ -103,7 +93,7 @@ class RoutesController < ApplicationController
     response = http.request(request)
     hash = JSON.parse(response.body)
     start_time = hash["items"][0]["summary"]["move"]["from_time"]
-    @start_time = Time.parse(start_time).strftime('%-m/%d %H:%M')
+    @start_time = Time.parse(start_time).strftime('%H:%M')
     
     #ルートの情報を取得
     @route_info = hash["items"][0]["sections"]
